a414a586c37177e830bea36c9557893e90e522c4
Oleksandr P <goodwin009@gmail.com>
2022-09-29 14:02:59 +0300

    add solution 1,2 tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+refs.gallery.innerHTML = galleryItemsMarkup;
+
+refs.gallery.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(e) {
+  e.preventDefault();
+  const activeImg = e.target;
+  if (activeImg.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${activeImg.dataset.source}" width="900" height="600">`,
+    {
+      onShow: addListenerToOpenImg(),
+    },
+  );
+  instance.show();
+
+  function onKeyPressEscapeCloseImg(e) {
+    if (e.code === 'Escape') {
+      instance.close();
+      refs.gallery.removeEventListener('keydown', onKeyPressEscapeCloseImg);
+    }
+  }
+
+  function addListenerToOpenImg() {
+    refs.gallery.addEventListener('keydown', onKeyPressEscapeCloseImg);
+  }
+}
+
+function createGalleryItemsMarkup(el) {
+  return el
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`,
+    )
+    .join('');
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+const galleryElMarkup = addGalleryElementsMarkup(galleryItems);
+refs.gallery.innerHTML = galleryElMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function addGalleryElementsMarkup(el) {
+  return el
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`,
+    )
+    .join('');
+}
